70aa5f9a62d132c36d4e02d6dbc232a53bdae62f
malamanjula <47377822+malamanjula@users.noreply.github.com>
2019-02-06 12:26:03 +0530

    Create beg3.java

---
+++ beg3.java
@@ -0,0 +1,38 @@
+import java.util.Scanner;
+public class beg3
+{
+   public static void main(String[] arg)
+   {
+	boolean isVowel=false;;
+	Scanner scanner=new Scanner(System.in);
+	System.out.println("Input:");
+	char ch=scanner.next().charAt(0); 
+	System.out.println(ch);
+System.out.println("Output:");
+	scanner.close();
+	switch(ch)
+	{
+	   case 'a' :
+	   case 'e' :
+       case 'i' :
+	   case 'o' :
+	   case 'u' :
+	   case 'A' :
+	   case 'E' :
+	   case 'I' :
+	   case 'O' :
+	   case 'U' : isVowel = true;
+	}
+	if(isVowel == true) 
+	{
+	   System.out.println("Vowel");
+	}
+	else 
+	{
+	   if((ch>='a'&&ch<='z')||(ch>='A'&&ch<='Z'))
+		System.out.println("Consonant");
+	     else
+		   System.out.println("invalid");		
+        }
+   }
+}
