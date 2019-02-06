127db71c318d0cecc594e7991659415fe861f70f
malamanjula <47377822+malamanjula@users.noreply.github.com>
2019-02-06 14:29:08 +0530

    Create beg6.java

---
+++ beg6.java
@@ -0,0 +1,33 @@
+import java.util.Scanner;
+public class beg6
+{
+    public static void main(String[] args)
+    {
+        int year;
+        boolean leap = false;
+        System.out.print("Input:");
+        Scanner in = new Scanner(System.in);
+        year=in.nextInt();
+        System.out.print("\n"+year+"\n");
+        System.out.println("Output:");
+        if(year % 4 == 0)
+        {
+            if( year % 100 == 0)
+            {
+                if ( year % 400 == 0)
+                    leap = true;
+                else
+                    leap = false;
+            }
+            else
+                leap = true;
+        }
+        else
+            leap = false;
+
+        if(leap)
+            System.out.println("leap year");
+        else
+            System.out.println("Not");
+    }
+}
