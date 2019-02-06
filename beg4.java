ab01ededeba278679ac963b222e5b0de322b064d
malamanjula <47377822+malamanjula@users.noreply.github.com>
2019-02-06 13:49:10 +0530

    Update beg4.java

--- beg4.java
+++ beg4.java
@@ -5,8 +5,7 @@ class beg4
     {
         char ch;
         Scanner scan = new Scanner(System.in);
-		
-        System.out.print("Input:");
+	System.out.print("Input:");
         ch=scan.next().charAt(0);
         System.out.print("\n"+ch+"\n");
         System.out.println("Output:");
