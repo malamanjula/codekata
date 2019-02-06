684d32999f1c957d6d7539c5499d3585e34ce8ee
malamanjula <47377822+malamanjula@users.noreply.github.com>
2019-02-06 15:49:56 +0530

    Create beg10.java

---
+++ beg10.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+class beg10
+{
+private static Scanner sc;
+public static void main(String[] args)
+{
+int Number,Count=0;
+sc=new Scanner(System.in);
+Number=sc.nextInt();
+while(Number>0)
+{
+Number=Number/10;
+Count=Count+1;
+}
+System.out.println(Count)
+}
+}
