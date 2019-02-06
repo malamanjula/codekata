f29f808a35bc4b50dccd2fabe4b318f572e4076e
malamanjula <47377822+malamanjula@users.noreply.github.com>
2019-02-06 11:30:25 +0530

    Create beg1.java

---
+++ beg1.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+public class beg1
+{
+    public static void main(String[] args)
+    {
+        int number;
+        Scanner s=new Scanner(System.in);
+        System.out.println("Input");
+        number=s.nextInt();
+        System.out.println(number);
+        if(number>0)
+        {
+            System.out.println("Output:");
+            System.out.println("Positive");
+        }
+        else if(number<0)
+        {
+            System.out.println("Output:");
+            System.out.println("Negative");
+        }
+        else
+        {
+            System.out.println("Output:");
+            System.out.println("Zero");
+        }
+    }
+}
