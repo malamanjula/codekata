2636401af4bef8a2baab50ba050702b3f40de130
malamanjula <47377822+malamanjula@users.noreply.github.com>
2019-02-06 15:18:40 +0530

    Create beg8.java

---
+++ beg8.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+class beg8
+{
+    public static void main(String args[])
+    {
+        int x, i = 1 ;
+        int sum = 0;
+        Scanner s = new Scanner(System.in);
+        x = s.nextInt();
+        while(i <= x)
+        {
+            sum = sum +i;
+            i++;
+        }
+        System.out.println(sum);
+    }
+}
