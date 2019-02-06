e29525ee4197a35e46c82f0fa0dcedf9c04fed7b
malamanjula <47377822+malamanjula@users.noreply.github.com>
2019-02-06 16:06:58 +0530

    Create beg11.java

---
+++ beg11.java
@@ -0,0 +1,13 @@
+import java.io.*;
+import java.util.Scanner;
+public class beg11
+{
+public static void main(String args[])
+{
+int value,power;
+Scanner sc=new Scanner(System.in);
+value=sc.nextInt();
+power=sc.nextInt();
+System.out.println("OUTPUT="+(value^power));
+}
+}
