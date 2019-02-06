7c8a63ab452009b8d85dc6428808624eaefe0e52
malamanjula <47377822+malamanjula@users.noreply.github.com>
2019-02-06 15:10:24 +0530

    Create beg7.java

---
+++ beg7.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+class beg7
+{
+public static void main(String args[])
+{
+int n,i;
+Scanner in=new Scanner(System.in);
+n=in.nextInt();
+for(i=1;i<=n;i++)
+{
+System.out.println("Hello");
+}
+}
+}
