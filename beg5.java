5dd27d68cccbf544f4fef2416484ba292b4c375d
malamanjula <47377822+malamanjula@users.noreply.github.com>
2019-02-06 14:14:49 +0530

    Update beg5.java

--- beg5.java
+++ beg5.java
@@ -4,13 +4,13 @@ class Largest
     public static void main(String args[])
     {
         int x, y, z;
-        System.out.println("Input:\n");
+        System.out.println("Input:");
         Scanner in = new Scanner(System.in);
         x = in.nextInt();
         y = in.nextInt();
         z = in.nextInt();
         System.out.println(x+" " + y +" "+ z);
-        System.out.println("Output:\n");
+        System.out.println("Output:");
         if (x > y && x > z)
         {
           System.out.println(x);
