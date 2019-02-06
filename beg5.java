97470a2daade6e17a7fc44469281cfa99e5f33c7
malamanjula <47377822+malamanjula@users.noreply.github.com>
2019-02-06 14:10:28 +0530

    Create beg5.java

---
+++ beg5.java
@@ -0,0 +1,31 @@
+import java.util.Scanner;
+public class Largest
+{
+    public static void main(String args[])
+    {
+        int x, y, z;
+        System.out.println("Input:");
+        Scanner in = new Scanner(System.in);
+        x = in.nextInt();
+        y = in.nextInt();
+        z = in.nextInt();
+        System.out.println(x+" " + y +" "+ z);
+        System.out.println("Output:");
+        if (x > y && x > z)
+        {
+          System.out.println(x);
+        }
+        else if (y > x && y > z)
+        {
+          System.out.println(y);
+        }
+        else if (z > x && z > y)
+        {
+          System.out.println(z);
+        }
+        else
+        {
+          System.out.println("invalid");
+        }
+    }
+}
