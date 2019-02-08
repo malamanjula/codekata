4e85d25858088b45cbf8603c0ed719977e5340db
malamanjula <47377822+malamanjula@users.noreply.github.com>
2019-02-08 15:57:40 +0530

    Create beg14.java

---
+++ beg14.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+class beg14
+{
+public static void main(String[] args)
+{
+int a,b;
+Scanner in=new Scanner(System.in);
+a=in.nextInt();
+b=in.nextInt();
+for(i=a;i<=b;i++)
+{
+if(i%2!=0)
+{
+System.out.println(i+ "");
+}
+}
+}
+}
