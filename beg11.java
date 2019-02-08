ff7fe86adb1f6729670a8f464cbe559b31c1ce04
malamanjula <47377822+malamanjula@users.noreply.github.com>
2019-02-08 15:18:56 +0530

    Update beg11.java

--- beg11.java
+++ beg11.java
@@ -1,13 +1,27 @@
-import java.io.*;
 import java.util.Scanner;
-public class beg11
+class beg11
 {
 public static void main(String args[])
 {
-int value,power;
+long n,p,r=1;
 Scanner sc=new Scanner(System.in);
-value=sc.nextInt();
-power=sc.nextInt();
-System.out.println("OUTPUT="+(value^power));
+n=sc.nextLong();
+p=sc.nextLong();
+  if(n>=0&&p==0)
+  {
+    r=1;
+  }
+  else if(n==0&&p=1)
+  {
+    r=0;
+  }
+  else
+  {
+    for(int i=1;i<=p;i++)
+    {
+      r=r*n;
+    }
+  }
+System.out.println(r);
 }
 }
