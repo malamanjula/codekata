f319074a584301f39911f93041d76d6e3f0dd202
malamanjula <47377822+malamanjula@users.noreply.github.com>
2019-03-07 22:16:31 +0530

    Update beg21.java

--- beg21.java
+++ beg21.java
@@ -1,37 +1,22 @@
 import java.util.Scanner; 
 
 class sap{  
-
-    static int sumOfAP(int a, int d, int n){ 
-
+    static int sumOfAP(int a, int d, int n)
+    { 
         int sum = 0; 
-
-        for (int i = 0; i < n; i++) 
-
+        for (int i = 0; i < n; i++)
         { 
-
             sum = sum + a; 
-
-            a = a + d; 
-
+            a = a + d;
         } 
-
         return sum; 
-
     } 
-
     public static void main(String args[]) {  
 
-        Scanner s = new Scanner(System.in);  
-
+        Scanner s = new Scanner(System.in); 
         int n = s.nextInt();
-
         int a = s.nextInt();
-
         int d = s.nextInt();
-
-        System.out.println(sumOfAP(a, d, n)); 
-
+        System.out.println(sumOfAP(a, d, n));
     } 
-
 } 
