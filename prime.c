7766fcd44d85609607b1cf06a2a8035b7f7b58b4
malamanjula <47377822+malamanjula@users.noreply.github.com>
2019-03-07 21:06:48 +0530

    Update prime.c

--- prime.c
+++ prime.c
@@ -9,13 +9,13 @@ int main()
         flag = 0;
         for(i = 2; i <= low/2; ++i)
         {
-            if(low % i == 0)
+            if(low%i==0)
             {
                 flag = 1;
                 break;
             }
         }
-        if (flag == 0)
+        if (flag==0)
             printf("%d ", low);
         low++;
     }
