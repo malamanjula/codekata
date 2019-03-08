515e9f4a0d9eeae174ae047942eb19ec2d62d901
malamanjula <47377822+malamanjula@users.noreply.github.com>
2019-03-08 07:09:33 +0530

    Create beg29.java

---
+++ beg29.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+class hour{
+    public static void main(String[] args)
+    {
+        int minute;
+        Scanner s = new Scanner(System.in); 
+         minute= s.nextInt();
+         System.out.println((minute/60)+" "+(minute%60));
+    }
+}
