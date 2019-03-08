39e027071cac3783aa160258f09ccf704856a13f
malamanjula <47377822+malamanjula@users.noreply.github.com>
2019-03-08 07:01:17 +0530

    Create beg26.java

---
+++ beg26.java
@@ -0,0 +1,18 @@
+import java.util.*;
+import java.lang.*;
+class sort
+{
+      public static void main(String arg[]){
+        int n;
+        Scanner s=new Scanner(System.in);
+        n=s.nextInt();
+        int a[]=new int[n];
+        for(int i=0;i<n;i++){
+            a[i]=s.nextInt();}
+       Arrays.sort(a);
+       int t=n/2;
+       for(int i=0;i<n;i++)
+       {      
+               System.out.print(a[i]+" ");    }      
+}
+}
