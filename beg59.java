435f717e00b1469c2b1a6a698980745a2f325f89
malamanjula <47377822+malamanjula@users.noreply.github.com>
2019-03-08 12:10:50 +0530

    Create beg59.java

---
+++ beg59.java
@@ -0,0 +1,16 @@
+import java.util.*;
+class count
+{
+public static void main(String args[])
+{
+Scanner SC=new Scanner(System.in);
+int num=SC.nextInt();
+int count=0;
+while(num!=0)
+{
+num/=10;
+count++;
+}
+System.out.print(count);
+}
+}
