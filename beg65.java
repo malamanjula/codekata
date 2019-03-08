cca369fcb392b1b6e6e08efb7cc66b5b255c20da
malamanjula <47377822+malamanjula@users.noreply.github.com>
2019-03-08 15:11:10 +0530

    Update beg65.java

--- beg65.java
+++ beg65.java
@@ -1,7 +1,10 @@
+import java.util.*;
 class hello
 {
 public static void main(String[] args) 
 {
-if(System.out.printf("hello world") == null) {}
+  Scanner s=new Scanner(System.in);
+  String str=new String();
+if(System.out.printf(str) == null) {}
 }
 }
