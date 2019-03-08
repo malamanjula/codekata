e07835daa95a2faf94aac01b4cfec9e32dad0cf0
malamanjula <47377822+malamanjula@users.noreply.github.com>
2019-03-08 07:02:46 +0530

    Update beg26.java

--- beg26.java
+++ beg26.java
@@ -13,6 +13,7 @@ class sort
        int t=n/2;
        for(int i=0;i<n;i++)
        {      
-               System.out.print(a[i]+" ");    }      
+               System.out.print(a[i]);    
+       }      
 }
 }
